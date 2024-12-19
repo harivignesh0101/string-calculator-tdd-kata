0a1cbbad53d8a48c823bb76b555f9a7804fb631f
hari-0101 <harivignesh@apiplatform.io>
2024-12-19 18:29:07 +0530

    modified add() to work for any amount of number in the string

--- src/main/java/com/harivignesh/StringCalculator.java
+++ src/main/java/com/harivignesh/StringCalculator.java
@@ -6,8 +6,8 @@ public class StringCalculator {
      * Note - Incomplete method.<br/>
      * Adds the numbers provided in a string format.<br/>
      * If the input string is empty, the method returns 0.
-     * Otherwise, it splits the string to list of comma stripped values and returns the single number
-     * if only one part is there or else returns the sum of 1st and 2nd index.
+     * Otherwise, it splits the string to list of comma stripped values and returns the sum of the parts
+     * regardless of the number of Numbers.
      *
      * @param numbers the string containing the numbers to be added
      * @return the sum of the numbers as an integer, or 0 if the string is empty
@@ -17,10 +17,11 @@ public class StringCalculator {
             return 0;
         }
         String[] parts = numbers.split(",");
-        if (parts.length == 1) {
-            return Integer.parseInt(parts[0]);
+        int sum = 0;
+        for (String part : parts) {
+            sum += Integer.parseInt(part);
         }
-        return Integer.parseInt(parts[0]) + Integer.parseInt(parts[1]);
+        return sum;
     }
 
 }
