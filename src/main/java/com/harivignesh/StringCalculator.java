36f985c0288bc2226b847a9ed5f06c319acacf0f
hari-0101 <harivignesh@apiplatform.io>
2024-12-19 18:15:43 +0530

    added logic for handling single number, testSingleNumber() works now

--- src/main/java/com/harivignesh/StringCalculator.java
+++ src/main/java/com/harivignesh/StringCalculator.java
@@ -6,7 +6,7 @@ public class StringCalculator {
      * Note - Incomplete method.<br/>
      * Adds the numbers provided in a string format.<br/>
      * If the input string is empty, the method returns 0.
-     * Returns 0 for all cases right now.
+     * Otherwise, it parses the string to an integer and returns the result.
      *
      * @param numbers the string containing the numbers to be added
      * @return the sum of the numbers as an integer, or 0 if the string is empty
@@ -15,7 +15,7 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         }
-        return 0;
+        return Integer.parseInt(numbers);
     }
 
 }
