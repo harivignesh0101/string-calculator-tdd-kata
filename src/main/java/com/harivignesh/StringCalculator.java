f9ac0da95fff074786fc7c91a4869418e722cff1
hari-0101 <harivignesh@apiplatform.io>
2024-12-19 18:21:20 +0530

    modified add() logic to work for single and 2 numbers

--- src/main/java/com/harivignesh/StringCalculator.java
+++ src/main/java/com/harivignesh/StringCalculator.java
@@ -6,7 +6,8 @@ public class StringCalculator {
      * Note - Incomplete method.<br/>
      * Adds the numbers provided in a string format.<br/>
      * If the input string is empty, the method returns 0.
-     * Otherwise, it parses the string to an integer and returns the result.
+     * Otherwise, it splits the string to list of comma stripped values and returns the single number
+     * if only one part is there or else returns the sum of 1st and 2nd index.
      *
      * @param numbers the string containing the numbers to be added
      * @return the sum of the numbers as an integer, or 0 if the string is empty
@@ -15,7 +16,11 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         }
-        return Integer.parseInt(numbers);
+        String[] parts = numbers.split(",");
+        if (parts.length == 1) {
+            return Integer.parseInt(parts[0]);
+        }
+        return Integer.parseInt(parts[0]) + Integer.parseInt(parts[1]);
     }
 
 }
