ccc14eecd6d67bb5cdb2e3835a1550ae3aa2e2cc
hari-0101 <harivignesh@apiplatform.io>
2024-12-19 17:55:38 +0530

    project setup - add junit lib, StringCalculator test, class definitions

---
+++ src/main/java/com/harivignesh/StringCalculator.java
@@ -0,0 +1,4 @@
+package com.harivignesh;
+
+public class StringCalculator {
+}
---
+++ src/test/java/com/harivignesh/StringCalculatorTest.java
@@ -0,0 +1,5 @@
+package com.harivignesh;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+}
