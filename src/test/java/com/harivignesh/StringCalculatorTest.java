69bd0a3884e77badaba006e17b40d9c9de46edda
hari-0101 <harivignesh@apiplatform.io>
2024-12-19 17:58:17 +0530

    added the first testcase 'testEmptyString' - contains syntax error

--- src/test/java/com/harivignesh/StringCalculatorTest.java
+++ src/test/java/com/harivignesh/StringCalculatorTest.java
@@ -1,5 +1,12 @@
 package com.harivignesh;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringCalculatorTest {
+    @Test
+    public void testEmptyString() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0, calculator.add(""), "Empty string should return 0");
+    }
 }
